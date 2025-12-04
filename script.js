d1fedbf67f38122ebe9c7d5f349074637ab94edf
11prath <90514562+11prath@users.noreply.github.com>
2025-12-04 10:37:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your JS code here. If required.
+document.getElementById("voteForm").addEventListener("submit", function (e) {
+  e.preventDefault(); // prevent page refresh
+
+  const age = document.getElementById("age").value;
+  const name = document.getElementById("name").value;
+
+  // Validation
+  if (!age || !name) {
+    alert("Please enter valid details.");
+    return;
+  }
+
+  // Creating a promise
+  const votePromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (parseInt(age) > 18) {
+        resolve();
+      } else {
+        reject();
+      }
+    }, 4000);
+  });
+
+  // Handling promise output
+  votePromise
+    .then(() => {
+      alert(`Welcome, ${name}. You can vote.`);
+    })
+    .catch(() => {
+      alert(`Oh sorry ${name}. You aren't old enough.`);
+    });
+});
